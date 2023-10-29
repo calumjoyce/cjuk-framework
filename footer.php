e55b231cdd105732343c5293fda37af38e185c9f
calumjoyce <calumjoyce74@gmail.com>
2023-10-29 12:01:25 +0000

    build: create and develop footer

--- footer.php
+++ footer.php
@@ -3,9 +3,36 @@
 		</div>
 	</div>
 
-	<footer id="footer" class="site-footer">
+	<footer class="py-20 bg-white">
+		<div class="module-container">
+			<div class="flex flex-col justify-center text-center">
+				<div class="mb-4">
+					<?php $site_title = get_bloginfo('name'); ?>
+					<h3 class="text-black text-4xl font-bold"><?php echo $site_title; ?>
+				</div>
+				<div class="mb-4 max-w-[600px] mx-auto">
+					<?php echo the_field('built_with', 'options'); ?>
+				</div>
+				<div class="flex items-center justify-center gap-6">
+					<a href="mailto:<?php the_field('email', 'options'); ?>">
+						<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+							<path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
+						</svg>
+					</a>
+					<a href="tel:<?php the_field('phone', 'options'); ?>">
+						<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+							<path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+						</svg>
+					</a>
+					<a href="<?php the_field('linkedin', 'options'); ?>">
+						<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+							<path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
+						</svg>
+					</a>
 
-		
+				</div>
+			</div>
+		</div>
 	</footer>
 
 </div>
